89216e4d31e4e6af06115919aeeed66b77814442
Blake Haug <blake@blakehaug.com>
2022-08-29 14:42:55 -0700

    Add team display and clean up code

--- lib/main.dart
+++ lib/main.dart
@@ -41,15 +41,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -60,13 +51,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Events _events = Events();
 
   Events events = Events();
-
-  // final getEventsThrottled = throttle(
-  //   () => {
-  //     Request.getEventList().then((value) => events = value),
-  //   },
-  //   const Duration(seconds: 2),
-  // );
 
   @override
   void initState() {
@@ -117,16 +101,6 @@ class _MyHomePageState extends State<MyHomePage> {
       body: ListView(
         padding: const EdgeInsets.all(8),
         children: <Widget>[
-          // Container(
-          //   height: 50,
-          //   margin: const EdgeInsets.all(4),
-          //   child: ElevatedButton(
-          //     onPressed: () {
-          //       printThrottled();
-          //     },
-          //     child: const Text('Refresh'),
-          //   ),
-          // ),
           if (_events.data != null)
             for (var event in _events.data!)
               InkWell(
--- lib/match.dart
+++ lib/match.dart
@@ -8,15 +8,6 @@ class MatchPage extends StatefulWidget {
       {Key? key, required this.title, required this.event_old, required this.match_number})
       : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
   final Event event_old;
   final int match_number;
@@ -58,24 +49,6 @@ class _MatchPageState extends State<MatchPage> {
       const Duration(seconds: 2),
     );
 
-    if (event.divisions != null) {
-      for (var i = 0;
-          i < (event.divisions?[0].data?.data?[widget.match_number].alliances?.length ?? 1) - 1;
-          i++) {
-        for (var f = 0;
-            f <
-                (((event.divisions?[0].data?.data?[widget.match_number].alliances?[i])
-                        ?.teams
-                        ?.length ??
-                    0));
-            i++) {
-          print((event.divisions?[0].data?.data?[widget.match_number].alliances?[i].teams?[f].team
-                  ?.name)
-              .toString());
-        }
-      }
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -89,24 +62,11 @@ class _MatchPageState extends State<MatchPage> {
         ],
       ),
       body: ListView(padding: const EdgeInsets.all(8), children: <Widget>[
-        Text((event.divisions?[0].data?.data?[widget.match_number].id).toString()),
-        // for (var i = 0;
-        //     i <
-        //         (event.divisions?[0].data?.data?[widget.match_number].alliances
-        //                 ?.length ??
-        //             1);
-        //     i++)
-        //   for (var f = 0;
-        //       f <
-        //           (((event.divisions?[0].data?.data?[widget.match_number]
-        //                       .alliances?[i])
-        //                   ?.teams
-        //                   ?.length ??
-        //               1));
-        //       i++)
-        //     Text((event.divisions?[0].data?.data?[widget.match_number]
-        //             .alliances?[i].teams?[f].team?.name)
-        //         .toString()),
+        if (event.divisions != null)
+          if (event.divisions?[0].data?.data?[widget.match_number].alliances?.length != null)
+            for (var prop in (event.divisions![0].data!.data![widget.match_number].alliances!))
+              if (prop.teams != null)
+                for (var prop2 in prop.teams!) Text((prop2.team?.name).toString()),
       ]),
     );
   }
