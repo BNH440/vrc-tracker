dcef9ad1974d2d6aa2ab49bdb208ef0cd087626e
Blake Haug <blake@blakehaug.com>
2022-10-11 08:38:13 -0700

    Add const to theme

--- lib/themeData.dart
+++ lib/themeData.dart
@@ -5,22 +5,21 @@ class Styles {
     return ThemeData(
       primarySwatch: Colors.red,
       colorScheme: isDarkTheme
-          ? ColorScheme.dark(primary: Colors.red, secondary: Colors.red)
-          : ColorScheme.light(primary: Colors.red, secondary: Colors.red),
+          ? const ColorScheme.dark(primary: Colors.red, secondary: Colors.red)
+          : const ColorScheme.light(primary: Colors.red, secondary: Colors.red),
       primaryColor: isDarkTheme ? Colors.black : Colors.white,
-      backgroundColor: isDarkTheme ? Colors.black : Color(0xffF1F5FB),
-      indicatorColor: isDarkTheme ? Color(0xff0E1D36) : Color(0xffCBDCF8),
-      hintColor: isDarkTheme ? Color(0xff280C0B) : Color(0xffEECED3),
-      highlightColor: isDarkTheme ? Color(0xff372901) : Color(0xffFCE192),
-      hoverColor: isDarkTheme ? Color(0xff3A3A3B) : Color(0xff4285F4),
-      focusColor: isDarkTheme ? Color(0xff0B2512) : Color(0xffA8DAB5),
+      backgroundColor: isDarkTheme ? Colors.black : const Color(0xffF1F5FB),
+      indicatorColor: isDarkTheme ? const Color(0xff0E1D36) : const Color(0xffCBDCF8),
+      hintColor: isDarkTheme ? const Color(0xff280C0B) : const Color(0xffEECED3),
+      highlightColor: isDarkTheme ? const Color(0xff372901) : const Color(0xffFCE192),
+      hoverColor: isDarkTheme ? const Color(0xff3A3A3B) : const Color(0xff4285F4),
+      focusColor: isDarkTheme ? const Color(0xff0B2512) : const Color(0xffA8DAB5),
       disabledColor: Colors.grey,
       cardColor: isDarkTheme ? Colors.grey[850] : Colors.grey[300],
       canvasColor: isDarkTheme ? Colors.black : Colors.grey[50],
       brightness: isDarkTheme ? Brightness.dark : Brightness.light,
-      buttonTheme: Theme.of(context)
-          .buttonTheme
-          .copyWith(colorScheme: isDarkTheme ? ColorScheme.dark() : ColorScheme.light()),
+      buttonTheme: Theme.of(context).buttonTheme.copyWith(
+          colorScheme: isDarkTheme ? const ColorScheme.dark() : const ColorScheme.light()),
       appBarTheme: const AppBarTheme(
         elevation: 0.0,
       ),
