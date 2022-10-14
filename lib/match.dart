e1659509d8ac8159987284f930d414d105d8b556
Blake Haug <blake@blakehaug.com>
2022-10-14 10:22:36 -0700

    Add error handling for match page

--- lib/match.dart
+++ lib/match.dart
@@ -2,9 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:rate_limiter/rate_limiter.dart';
+import 'package:vrc_ranks_app/Schema/Division.dart' as Div;
 import 'package:vrc_ranks_app/Schema/Events.dart';
 import 'package:vrc_ranks_app/team.dart';
 import 'Request.dart' as Request;
+import 'package:collection/collection.dart';
 
 class MatchPage extends StatefulWidget {
   const MatchPage(
@@ -52,15 +54,14 @@ class _MatchPageState extends State<MatchPage> {
       const Duration(seconds: 2),
     );
 
-    var match = (event.divisions?[0].data?.data?.singleWhere(
-        (element) => element.id == widget.match_number,
-        orElse: () => event.divisions![0].data!.data![0]));
+    Div.Data? match = (event.divisions?[0].data?.data
+        ?.firstWhereOrNull((element) => element.id == widget.match_number));
 
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: (match).toString() == "null"
+      body: (event.divisions?[0].data?.data).toString() == "null"
           ? const Align(
               alignment: Alignment.topCenter,
               child: Padding(
@@ -71,107 +72,110 @@ class _MatchPageState extends State<MatchPage> {
               ),
             )
           : RefreshIndicator(
-              child: ListView(padding: const EdgeInsets.all(8), children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Theme.of(context).cardColor,
-                  ),
-                  padding: const EdgeInsets.all(8),
-                  margin: const EdgeInsets.all(4),
-                  child: Flex(direction: Axis.vertical, children: [
-                    ListView(
-                      shrinkWrap: true,
-                      padding: const EdgeInsets.all(8),
-                      physics: const NeverScrollableScrollPhysics(),
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 10),
-                          child: Text(
-                            match!.name.toString(),
-                            style: const TextStyle(fontSize: 20),
-                          ),
+              child: match == null
+                  ? const Center(child: Text("Match not found"))
+                  : ListView(padding: const EdgeInsets.all(8), children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Theme.of(context).cardColor,
                         ),
-                        Text(
-                          "Field: ${match.field.toString()}",
-                          style: const TextStyle(fontSize: 15),
-                        ),
-                        Text(
-                          "Scheduled: ${DateFormat.jm().format(DateTime.parse(match.scheduled.toString()))}",
-                          style: const TextStyle(fontSize: 15),
-                        ),
-                        if (match.started != null)
-                          Text(
-                            "Started: ${DateFormat.jm().format(DateTime.parse(match.started.toString()))}",
-                            style: const TextStyle(fontSize: 15),
-                          ),
-                        Text(
-                          "Scored: ${match.scored.toString() == "true" ? "No" : "Yes"}",
-                          style: const TextStyle(fontSize: 15),
-                        ),
-                      ],
-                    ),
-                    if (!(match.scored ?? true))
-                      RichText(
-                        text: TextSpan(
-                          style: const TextStyle(
-                            fontSize: 25.0,
-                          ),
-                          children: <TextSpan>[
-                            TextSpan(
-                                text: match.alliances?[0].score.toString() ?? "",
-                                style: const TextStyle(color: Colors.blue)),
-                            TextSpan(
-                              text: " - ",
-                              style: TextStyle(color: Theme.of(context).textTheme.bodyLarge?.color),
-                            ),
-                            TextSpan(
-                                text: match.alliances?[1].score.toString() ?? "",
-                                style: const TextStyle(color: Colors.red)),
-                          ],
-                        ),
-                      )
-                  ]),
-                ),
-                if (event.divisions != null)
-                  if (match.alliances?.length != null)
-                    for (var prop in (match.alliances!))
-                      if (prop.teams != null)
-                        for (var prop2 in prop.teams!)
-                          InkWell(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                CupertinoPageRoute(
-                                    builder: (context) => TeamPage(
-                                        title: (prop2.team?.name).toString(),
-                                        match_id: event.id.toString(),
-                                        event_old: event,
-                                        team_id: (prop2.team?.id).toString())),
-                              );
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Theme.of(context).cardColor,
-                                border: Border.all(
-                                  color: prop.color == 'red'
-                                      ? Colors.red
-                                      : prop.color == 'blue'
-                                          ? Colors.blue
-                                          : Colors.grey,
-                                  width: 1,
+                        padding: const EdgeInsets.all(8),
+                        margin: const EdgeInsets.all(4),
+                        child: Flex(direction: Axis.vertical, children: [
+                          ListView(
+                            shrinkWrap: true,
+                            padding: const EdgeInsets.all(8),
+                            physics: const NeverScrollableScrollPhysics(),
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 10),
+                                child: Text(
+                                  match!.name.toString(),
+                                  style: const TextStyle(fontSize: 20),
                                 ),
                               ),
-                              height: 50,
-                              padding: const EdgeInsets.symmetric(horizontal: 30),
-                              margin: const EdgeInsets.all(4),
-                              child: Center(
-                                child: Text((prop2.team?.name).toString()),
+                              Text(
+                                "Field: ${match.field.toString()}",
+                                style: const TextStyle(fontSize: 15),
                               ),
-                            ),
+                              Text(
+                                "Scheduled: ${DateFormat.jm().format(DateTime.parse(match.scheduled.toString()))}",
+                                style: const TextStyle(fontSize: 15),
+                              ),
+                              if (match.started != null)
+                                Text(
+                                  "Started: ${DateFormat.jm().format(DateTime.parse(match.started.toString()))}",
+                                  style: const TextStyle(fontSize: 15),
+                                ),
+                              Text(
+                                "Scored: ${match.scored.toString() == "true" ? "No" : "Yes"}",
+                                style: const TextStyle(fontSize: 15),
+                              ),
+                            ],
                           ),
-              ]),
+                          if (!(match.scored ?? true))
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                  fontSize: 25.0,
+                                ),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                      text: match.alliances?[0].score.toString() ?? "",
+                                      style: const TextStyle(color: Colors.blue)),
+                                  TextSpan(
+                                    text: " - ",
+                                    style: TextStyle(
+                                        color: Theme.of(context).textTheme.bodyLarge?.color),
+                                  ),
+                                  TextSpan(
+                                      text: match.alliances?[1].score.toString() ?? "",
+                                      style: const TextStyle(color: Colors.red)),
+                                ],
+                              ),
+                            )
+                        ]),
+                      ),
+                      if (event.divisions != null)
+                        if (match.alliances?.length != null)
+                          for (var prop in (match.alliances!))
+                            if (prop.teams != null)
+                              for (var prop2 in prop.teams!)
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      CupertinoPageRoute(
+                                          builder: (context) => TeamPage(
+                                              title: (prop2.team?.name).toString(),
+                                              match_id: event.id.toString(),
+                                              event_old: event,
+                                              team_id: (prop2.team?.id).toString())),
+                                    );
+                                  },
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Theme.of(context).cardColor,
+                                      border: Border.all(
+                                        color: prop.color == 'red'
+                                            ? Colors.red
+                                            : prop.color == 'blue'
+                                                ? Colors.blue
+                                                : Colors.grey,
+                                        width: 1,
+                                      ),
+                                    ),
+                                    height: 50,
+                                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                                    margin: const EdgeInsets.all(4),
+                                    child: Center(
+                                      child: Text((prop2.team?.name).toString()),
+                                    ),
+                                  ),
+                                ),
+                    ]),
               onRefresh: () async {
                 await getEventDetailsThrottled();
               }),
